faa79b60f7d0b0a4c9bdf55209c8754e6704512b
PINTFI <filipesmpinto@gmail.com>
2020-05-03 17:53:26 +0200

    finish nav toggle

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.navi-links');
+    const navLinks = document.querySelectorAll('.navi-links li');
 
     burger.onclick = () => {
         nav.classList.toggle('navi-active');
     }
+
+    navLinks.forEach((link, index) => {
+        link.style.animation = 'navLinkFade 0.5s ease forwards';
+        console.log(index / 7);
+
+    });
 }
 
 navSlide();
